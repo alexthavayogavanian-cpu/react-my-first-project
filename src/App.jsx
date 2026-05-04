74428fa280d4275e7dc4c442d3cd8405aa3a1cf1
alexthava99-cyber <alexthava99@gmail.com>
2026-05-04 20:18:23 +0200

    My first commit of my first React App

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import User from "./components/User";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  async function fetchUsers() {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/cederdorff/race/master/data/users.json",
+    );
+
+    const data = await response.json();
+    console.log(data);
+    setUsers(data);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <main className="app">
+      <h1>My First React App</h1>
+
+      <section className="grid">
+        {users.map((user) => (
+          <User
+            key={user.id}
+            name={user.name}
+            mail={user.mail}
+            image={user.image}
+          />
+        ))}
+      </section>
+    </main>
+  );
+}
+
+export default App;
